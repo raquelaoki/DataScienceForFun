1c334691f87b4a1a05806bb7d93d7e0eeee8987b
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2019-04-25 23:11:22 -0700

    Update RforDataScience01.R

--- RforDataScience/RforDataScience01.R
+++ RforDataScience/RforDataScience01.R
@@ -56,4 +56,93 @@ ggplot(data = mpg) +
 
 ggplot(data = mpg) + 
   geom_smooth(mapping = aes(x = displ, y = hwy))
-#https://r4ds.had.co.nz/data-visualisation.html#geometric-objects
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy),show.legend = FALSE) + 
+  geom_point() + geom_smooth()
+
+#exercises 
+#Interesting comparison of small changes in the graphics 
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy), show.legend = FALSE) + 
+  geom_point()+ geom_smooth(se = FALSE)
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy), show.legend = FALSE) + 
+  geom_point()+ geom_smooth(se = FALSE,mapping = aes(group = drv))
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv), show.legend = FALSE) + 
+  geom_point()+ geom_smooth(se = FALSE,mapping = aes(group = drv))
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy), show.legend = FALSE) + 
+  geom_point(mapping = aes(color = drv))+ geom_smooth(se = FALSE)
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy), show.legend = FALSE) + 
+  geom_point(mapping = aes(color = drv))+
+  geom_smooth(se = FALSE,mapping = aes(linetype= drv))
+
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy), show.legend = FALSE) + 
+  geom_point(mapping = aes(color = drv))
+
+#3.7 
+#Transformations 
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut))
+
+#produce the same plot 
+ggplot(data = diamonds) + 
+  stat_count(mapping = aes(x = cut))
+
+#bar plot from values in a table 
+demo <- tribble(
+  ~cut,         ~freq,
+  "Fair",       1610,
+  "Good",       4906,
+  "Very Good",  12082,
+  "Premium",    13791,
+  "Ideal",      21551
+)
+
+ggplot(data = demo) +
+  geom_bar(mapping = aes(x = cut, y = freq), stat = "identity")
+
+
+#summary in a plot (similar to a boxplot)
+ggplot(data = diamonds) + 
+  stat_summary( mapping = aes(x = cut, y = depth),
+    fun.ymin = min, 
+    fun.ymax = max,
+    fun.y = median
+  )
+
+#Exercises 
+#1) The default value is the mean()
+ggplot(data = diamonds) + 
+  stat_summary( mapping = aes(x = cut, y = depth) )
+
+
+#2) bar plot is able to make transformations, col don't 
+ggplot(data = demo) +
+  geom_bar(mapping = aes(x = cut, y = freq), stat = "identity")
+
+
+ggplot(data = demo) +
+  geom_col(mapping = aes(x = cut, y = freq))
+
+#3.8) Exercises 
+#1) we can improving adding 'jitter'/random noise 
+ggplot(data = mpg, mapping = aes(x = cty, y = hwy)) + 
+  geom_point(position = 'jitter')
+
+#4) boxplot
+ggplot(data = diamonds) + 
+  geom_boxplot( mapping = aes(x = cut, y = depth))
+
+
+
+#Chapter 5.6 
+#stop use order() and use arange(), the correct function to order rows
+#https://r4ds.had.co.nz/transform.html#grouped-summaries-with-summarise
+#I'm on chapter 5.6
